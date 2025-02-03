799d4be969f935197e09aa2f0ef913d004313bb4
George <ujuf2_2002@mail.ru>
2025-02-03 19:45:45 +0500

    for netlify

--- vite-project/src/components/Sidebar.tsx
+++ vite-project/src/components/Sidebar.tsx
@@ -1,6 +1,5 @@
 import { Menu } from "antd";
 import { Link } from "react-router-dom";
-import { AppstoreOutlined, UserOutlined } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 
 
