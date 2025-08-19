21e686546f405d7a106fa3c459c815759eb98d1f
George <ujuf2_2002@mail.ru>
2025-08-19 12:19:09 +0500

    add strict mode

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -6,8 +6,9 @@ import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
-  
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
+  </StrictMode>
 );
